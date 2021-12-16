ab1444d38acda6184ad75377836d9653ca6e96ea
alxitaliano <alx.italiano@gmail.com>
2021-12-16 15:45:19 -0300

    Retirei marcações de controle
    Removi as marcações de controle do funcionamento interno, servia apenas para acompanhar a navegação interna.

--- Login2/main_file.py
+++ Login2/main_file.py
@@ -8,6 +8,7 @@ from reportlab.lib.pagesizes import A4
 numero_id = None
 busca_id = None
 busca_id2 = None
+busca_id3 = None
 login_user = ""
 
 # DATABASE LINK
@@ -21,7 +22,6 @@ banco2 = mysql.connector.connect(
 # FUNCTIONS
 # START LOGIN
 def def_login():
-    print("tela de login")
     global login_user
     login_sc.label_status.setText("")
     nome_user = login_sc.lineEdit.text()
@@ -32,7 +32,6 @@ def def_login():
     result = cursor.fetchone()
     if result:
         login_user = nome_user
-        print("Usuário logado: ", nome_user)
         main_sc.show()
         def_home()
         main_sc.label_8.setText(login_user)
@@ -47,7 +46,6 @@ def def_home():
     main_sc.home.show()
     global busca_id
     global login_user
-    print("home")
 
     main_sc.label_8.setText(login_user)
     # batches
@@ -55,7 +53,6 @@ def def_home():
     comando_sql1 = ("SELECT lote, data_abert, data_fech, lote_status FROM controle_lote1 order by lote desc LIMIT 2")
     cursor.execute(comando_sql1)
     dados_lidos1 = cursor.fetchall()
-    print(dados_lidos1)
 
     main_sc.tableWidget.setRowCount(len(dados_lidos1))
     main_sc.tableWidget.setColumnCount(4)
@@ -71,7 +68,6 @@ def def_home():
     comando_sql2 = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas order by id desc LIMIT 3")
     cursor.execute(comando_sql2)
     dados_lidos = cursor.fetchall()
-    print(dados_lidos)
 
     main_sc.tableWidget_2.setRowCount(len(dados_lidos))
     main_sc.tableWidget_2.setColumnCount(7)
@@ -90,7 +86,6 @@ def def_users():
     main_sc.reports_id.close()
     main_sc.about_app.close()
     main_sc.users.show()
-    print("users")
 
 
 def def_list_users():
@@ -98,7 +93,6 @@ def def_list_users():
     comando_sql1 = ("SELECT data, nome, login, email FROM login order by id")
     cursor.execute(comando_sql1)
     dados_lidos1 = cursor.fetchall()
-    print(dados_lidos1)
 
     main_sc.tableWidget_6.setRowCount(len(dados_lidos1))
     main_sc.tableWidget_6.setColumnCount(4)
@@ -109,8 +103,6 @@ def def_list_users():
 
 
 def def_add_users():
-    print("add_users")
-
     nome = main_sc.lineEdit.text()
     login1 = main_sc.lineEdit_2.text()
     senha1 = main_sc.lineEdit_3.text()
@@ -121,7 +113,6 @@ def def_add_users():
         if senha1 == senha2:
             cursor = banco2.cursor()
             cursor.execute("Insert into login Values (null, now(), '" + nome + "','" + login1 + "','" + senha1 + "', '" + email + "')")
-            cursor.commit()
             main_sc.label_20.setText("Usuário cadastrado com Sucesso!!")
             main_sc.lineEdit.setText("")
             main_sc.lineEdit_2.setText("")
@@ -129,9 +120,10 @@ def def_add_users():
             main_sc.lineEdit_4.setText("")
             main_sc.lineEdit_5.setText("")
             def_list_users()
+        else:
+            main_sc.label_20.setText("Senhas não conferem")
     else:
         main_sc.label_20.setText("Preencha todos os campos | Usuário não cadastrado!")
-        print("usuário não cadastrado")
 
 
 def def_del_users():
@@ -155,7 +147,7 @@ def def_users_pdf():
     y = 0
     #user_windows = getpass.getuser()
     #pdf = canvas.Canvas(f'C:\\Users\\{user_windows}\\Desktop\\cadastro_usuarios.pdf', pagesize=A4)
-    pdf = canvas.Canvas("cadastro_usuarios.pdf", pagesize=A4)
+    pdf = canvas.Canvas("cadastro_usuarios.pdf")
     pdf.setFont("Times-Bold", 18)
     pdf.drawString(200, 800, "Usuários Cadastrados: ")
     # 200 é a distância do inicio do paragrafo levanto em conta a borda esquerda
@@ -177,7 +169,6 @@ def def_users_pdf():
 
     pdf.save()
     main_sc.label_20.setText("PDF Gerado com sucesso!!")
-    print("Status da função: pdf gerado")
 # END USERS
 
 
@@ -190,48 +181,80 @@ def def_batch():
     main_sc.id_card.close()
     main_sc.batch.show()
     def_list_batch()
-    print("lotes")
 
 
-def def_add_batch():
-    nlote = main_sc.lineEdit_9.text()
+def def_check_batch():# Regularizar função
+    nlote = int(main_sc.lineEdit_9.text())
+
+    cursor = banco2.cursor()
+    cursor.execute("SELECT lote FROM controle_lote1 order by lote desc")
+    dados_lidos = cursor.fetchone()
+    valor_id = dados_lidos[0]
+    print(type(valor_id))
+    print(type(nlote))
+
+    if valor_id != nlote:
+        cursor2 = banco2.cursor()
+        abrir = ("Insert into controle_lote1 (id, lote, data_abert, data_fech, lote_status) values (null,'" + str(nlote) + "', now(), null, 'Aberto')")
+        cursor2.execute(abrir)
+        main_sc.label_38.setText("Novo Lote Aberto")
+        def_list_batch()
+        print("Aberto")
+    else:
+        main_sc.label_38.setText("Lote já está aberto")
+        print("não deu")
+
+
+def def_add_batch(): # Regularizar função - Erro no if com insert ???
+    nlote = int(main_sc.lineEdit_9.text())
     print(nlote)
+    print(30*"-")
 
-    cursor2 = banco2.cursor()
-    abrir = ("Insert into controle_lote1 (id, lote, data_abert, data_fech, lote_status) values (null,'" + nlote + "', now(), null, 'Aberto')")
-    cursor2.execute(abrir)
-    cursor2.commit()
-    main_sc.label_38.setText("Novo Lote Aberto")
-    def_list_batch()
-    print("Aberto")
+    cursor = banco2.cursor()
+    cursor.execute("SELECT lote FROM controle_lote1 order by lote desc")
+    dados_lidos = cursor.fetchone()
+    valor_id = dados_lidos[0]
+    print(type(valor_id))
+    print(type(nlote))
+
+    if valor_id != nlote:
+        cursor2 = banco2.cursor()
+        abrir = ("Insert into controle_lote1 (id, lote, data_abert, data_fech, lote_status) values (null,'" + str(nlote) + "', now(), null, 'Aberto')")
+        cursor2.execute(abrir)
+        main_sc.label_38.setText("Novo Lote Aberto")
+        def_list_batch()
+        print("Aberto")
+    else:
+        main_sc.label_38.setText("Lote já está aberto")
+        print("não deu")
 
 
 def def_close_batch():
     nlote = main_sc.lineEdit_9.text()
-    print(nlote)
 
     cursor2 = banco2.cursor()
     consulta = ("Update controle_lote1 set data_fech = now(), lote_status = 'Fechado' where lote = '" + nlote + "' ")
     cursor2.execute(consulta)
     main_sc.label_38.setText("O lote selecionado foi fechado")
     def_list_batch()
-    print("Lote Fechado")
 
 
 def def_del_batch():
     linha = main_sc.tableWidget_4.currentRow()
-    main_sc.tableWidget_4.removeRow(linha)
-    print(linha)
-    user = "adm"
 
-    if user != "zé":
+    if linha != -1:
         cursor = banco2.cursor()
-        cursor.execute("SELECT id FROM controle_lote1 order by id desc")
+        cursor.execute("SELECT id, lote_status FROM controle_lote1 order by id desc")
         dados_lidos = cursor.fetchall()
         valor_id = dados_lidos[linha][0]
-        cursor.execute("DELETE FROM controle_lote1 WHERE id = " + str(valor_id))
-        main_sc.label_38.setText("O lote selecionado foi deletado")
-        def_list_batch()
+        valor_id2 = dados_lidos[linha][1]
+        if valor_id2 == "Aberto":
+            main_sc.label_38.setText("O lote selecionado está aberto e não pode ser deletado!")
+        else:
+            main_sc.tableWidget_4.removeRow(linha)
+            cursor.execute("DELETE FROM controle_lote1 WHERE id = " + str(valor_id))
+            main_sc.label_38.setText("O lote selecionado foi deletado")
+            def_list_batch()
     else:
         main_sc.label_38.setText("Escolha um lote ou vc não tem permissão!")
 
@@ -241,7 +264,6 @@ def def_list_batch():
     comando_sql1 = ("SELECT lote, data_abert, data_fech, lote_status FROM controle_lote1 order by lote desc")
     cursor.execute(comando_sql1)
     dados_lidos1 = cursor.fetchall()
-    print(dados_lidos1)
 
     main_sc.tableWidget_4.setRowCount(len(dados_lidos1))
     main_sc.tableWidget_4.setColumnCount(4)
@@ -258,19 +280,16 @@ def def_id_card():
     main_sc.users.close()
     main_sc.batch.close()
     main_sc.id_card.show()
-    print("carteirinhas")
 
 
 def def_add_idcard():
     global login_user
     login_user = "alxitaliano"
-    print(login_user)
 
     cursor = banco2.cursor()
     comando_sql2 = ("SELECT * FROM carteirinhas order by id desc LIMIT 7")
     cursor.execute(comando_sql2)
     dados_lidos = cursor.fetchall()
-    print(dados_lidos)
 
     main_sc.tableWidget_3.setRowCount(len(dados_lidos))
     main_sc.tableWidget_3.setColumnCount(7)
@@ -285,13 +304,10 @@ def def_add_idcard():
 
     tipo1 = ""
     if main_sc.radioButton.isChecked():
-        print("1ª Via")
         tipo1 = "1ª Via"
     elif main_sc.radioButton_2.isChecked():
-        print("2ª Via")
         tipo1 = "2ª Via"
     else:
-        print("Atualização de Foto")
         tipo1 = "Atualização de Foto"
 
     cursor = banco2.cursor()
@@ -300,8 +316,6 @@ def def_add_idcard():
     dados_lidos = cursor.fetchone()
     status = (str(dados_lidos[0]))
     if status == "Aberto":
-        # comando_SQL = ("INSERT INTO carteirinhas (nome,ra,tipo) VALUES (%s,%s,%s,%)")
-        # comando_sql = ("INSERT INTO carteirinhas3 (id, lote, data, nome, ra, tipo, user_cadastro) VALUES (null, '" + lote1 + "', now(),'" + nome1 + "','" + ra1 + "','" + tipo1 + "', '" + login_user + "')")
         comando_sql = ("INSERT INTO carteirinhas (id, lote, data, nome, ra, tipo, categoria, user_cadastro) VALUES (null, '" + lote1 + "', now(),'" + nome1 + "','" + ra1 + "','" + tipo1 + "', 'Aluno', '" + login_user + "')")
         # dados = (lote1, nome1, ra1, tipo1)
         cursor.execute(comando_sql)
@@ -320,15 +334,13 @@ def def_reports_id():
     main_sc.users.close()
     main_sc.batch.close()
     main_sc.id_card.close()
+    main_sc.about_app.close()
     main_sc.reports_id.show()
-    print("relatórios")
 
 
 def def_reports_all():
-    print("reports-all")
     main_sc.label_44.setText("")
     global busca_id
-    print(busca_id)
     #relcarteirinhas.show()
 
     if busca_id != "":
@@ -351,12 +363,10 @@ def def_reports_all():
 
 
 def def_reports_name():
-    print("reports-name")
     main_sc.label_44.setText("")
     global busca_id
     global busca_id2
-    print(busca_id)
-    print(busca_id2)
+    global busca_id3
     #relcarteirinhas.show()
 
     if busca_id2 == 'nome':
@@ -369,16 +379,22 @@ def def_reports_name():
         comando_sql2 = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where ra like '%" + busca_id + "%' order by ID desc")
         cursor.execute(comando_sql2)
         dados_lidos = cursor.fetchall()
+    elif busca_id2 == "lote":
+        cursor = banco2.cursor()
+        comando_sql2 = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where lote like '%" + busca_id + "%' order by ID desc")
+        cursor.execute(comando_sql2)
+        dados_lidos = cursor.fetchall()
     elif busca_id2 == "usuario":
         cursor = banco2.cursor()
         comando_sql2 = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where user_cadastro like '%" + busca_id + "%' order by ID desc")
         cursor.execute(comando_sql2)
         dados_lidos = cursor.fetchall()
-    else:
+    elif busca_id2 == "data":
         cursor = banco2.cursor()
-        comando_sql2 = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where lote like '%" + busca_id + "%' order by ID desc")
+        comando_sql2 = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where data between '" + busca_id + "' and '" + busca_id3 + "' order by data desc")
         cursor.execute(comando_sql2)
         dados_lidos = cursor.fetchall()
+
 
     main_sc.tableWidget_5.setRowCount(len(dados_lidos))
     main_sc.tableWidget_5.setColumnCount(7)
@@ -389,24 +405,21 @@ def def_reports_name():
 
 
 def def_reports_type():
-    print("reports-type")
     main_sc.label_44.setText("")
     global busca_id
     global busca_id2
+    global busca_id3
     # tipo1 = ""
 
     # if tipo1 == "teste":
     if main_sc.radioButton_4.isChecked():
         busca_id = "1ª Via"
-        print(busca_id)
         def_reports_all()
     elif main_sc.radioButton_5.isChecked():
         busca_id = "2ª Via"
-        print(busca_id)
         def_reports_all()
     elif main_sc.radioButton_6.isChecked():
         busca_id = "Atualização de foto"
-        print(busca_id)
         def_reports_all()
     elif main_sc.radioButton_8.isChecked():
         busca_id2 = "nome"
@@ -415,34 +428,33 @@ def def_reports_type():
     elif main_sc.radioButton_9.isChecked():
         busca_id2 = "ra"
         busca_id = main_sc.lineEdit_10.text()
-        print(busca_id)
         def_reports_name()
     elif main_sc.radioButton_10.isChecked():
         busca_id2 = "lote"
         busca_id = main_sc.lineEdit_10.text()
-        print(busca_id)
         def_reports_name()
     elif main_sc.radioButton_11.isChecked():
         busca_id2 = "usuario"
         busca_id = main_sc.lineEdit_10.text()
-        print(busca_id)
+        def_reports_name()
+    elif main_sc.radioButton_12.isChecked():
+        busca_id2 = "data"
+        busca_id = main_sc.dateEdit.text()
+        busca_id3 = main_sc.dateEdit_2.text()
         def_reports_name()
     else:
         busca_id = ""
-        print(busca_id)
         def_reports_all()
 
 
 def def_reports_date():
-    print("datas")
+    pass
 
 
 def def_reports_pdf():
-    print("pdf reports")
     global busca_id
     global busca_id2
-    print("ID Busca: ", busca_id)
-    print(30 * "-")
+    global busca_id3
     #main_sc.label_44.setText("")
 
     if busca_id == "":
@@ -478,8 +490,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso1!!")
     elif busca_id == '1ª Via':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where tipo = '" + busca_id + "' order by ID")
         cursor.execute(comando_sql)
@@ -512,8 +522,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
     elif busca_id == '2ª Via':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where tipo = '" + busca_id + "' order by ID")
         cursor.execute(comando_sql)
@@ -546,8 +554,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
     elif busca_id == 'Atualização de foto':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where tipo = '" + busca_id + "' order by ID")
         cursor.execute(comando_sql)
@@ -580,9 +586,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
     elif busca_id2 == 'nome':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
-        print("Busca id: ", busca_id2)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where nome like '%" + busca_id + "%' order by ID desc")
         #comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where nome = '" + busca_id + "' order by ID")
@@ -616,8 +619,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
     elif busca_id2 == 'ra':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where ra like '%" + busca_id + "%' order by ID desc")
         #comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where ra = '" + busca_id + "' order by ID")
@@ -651,9 +652,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
     elif busca_id2 == 'lote':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
-        print("Busca id2: ", busca_id2)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where lote like '%" + busca_id + "%' order by ID desc")
         #comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where lote = '" + busca_id + "' order by ID")
@@ -687,8 +685,6 @@ def def_reports_pdf():
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
     elif busca_id2 == 'usuario':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
         cursor = banco2.cursor()
         comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where user_cadastro like '%" + busca_id + "%' order by ID desc")
         #comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where user_cadastro = '" + busca_id + "' order by ID")
@@ -721,11 +717,9 @@ def def_reports_pdf():
 
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
-    elif busca_id == 'data':
-        print(30 * "-")
-        print("Busca id: ", busca_id)
+    elif busca_id2 == 'data':
         cursor = banco2.cursor()
-        comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where data = '" + busca_id + "' order by ID")
+        comando_sql = ("SELECT lote, data, nome, ra, tipo, categoria, user_cadastro FROM carteirinhas where data between '" + busca_id + "' and '" + busca_id3 + "' order by data")
         cursor.execute(comando_sql)
         dados_lidos = cursor.fetchall()
         y = 0
@@ -755,7 +749,6 @@ def def_reports_pdf():
 
         pdf.save()
         main_sc.label_44.setText("PDF Gerado com sucesso2!!")
-    print("Status da função: pdf gerado")
 # END REPORTS
 
 # ABOUT
@@ -766,7 +759,6 @@ def def_about_app():
     main_sc.id_card.close()
     main_sc.reports_id.close()
     main_sc.about_app.show()
-    print("sobre")
 
     global login_user
 
